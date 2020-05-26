9ab2616cb9e2456de0e355b62abb2ac7423cf3aa
Dong Min Lee <leegeobuk@gmail.com>
2020-05-27 04:15:59 +0900

    Fix writing to csv file in non-blocking way

--- write/write.go
+++ write/write.go
@@ -8,25 +8,30 @@ import (
 	"github.com/leegeobuk/jobscraper/util"
 )
 
-// Jobs writes all jobs on a csv file
+// Jobs writes all jobs to a csv file
 func Jobs(jobs []*job.Job) {
 	file, err := os.Create("jobs.csv")
 	util.CheckErr(err)
 
 	w := csv.NewWriter(file)
-	defer w.Flush()
+	// defer w.Flush()
 
 	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
 	wErr := w.Write(headers)
 	util.CheckErr(wErr)
 
+	c := make(chan []string)
 	for _, job := range jobs {
-		writeOnFile(w, job)
+		go parseJob(c, job)
+	}
+
+	for range jobs {
+		wErr := w.Write(<-c)
+		util.CheckErr(wErr)
 	}
 }
 
-func writeOnFile(w *csv.Writer, job *job.Job) {
+func parseJob(c chan<- []string, job *job.Job) {
 	jobSlice := job.ToSlice()
-	wErr := w.Write(jobSlice)
-	util.CheckErr(wErr)
+	c <- jobSlice
 }
